5ef0ae3471b35abf93838787b0ea87ba50fbd14e
tumzalehipi <brianmoipolai1@gmail.com>
2019-08-16 03:38:15 +0200

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
-class HomePageView(TemplateView):
-    
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 
 class AboutPageView(TemplateView):
     
